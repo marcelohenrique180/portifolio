6ba736f8be0131f4481578caf70e6a7acb1c65cf
Marcelo Henrique <marcelohs180@gmail.com>
2018-05-12 00:31:29 -0300

    make var name img as default

--- src/components/UnisinosItem/UnisinosItem.js
+++ src/components/UnisinosItem/UnisinosItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Item from '../Item/Item'
 
-import uniimg from './uni.gif'
+import img from './uni.gif'
 
 export default ({ showDetails }) => (
   <div onClick={() => { showDetails("Unisinos") }}>
-    <Item title="Iniciação Científica" img={uniimg} date="Atual" />
+    <Item title="Iniciação Científica" img={img} date="Atual" />
   </div>
 )
