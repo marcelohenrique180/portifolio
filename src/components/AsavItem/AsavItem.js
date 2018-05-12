e09ec09cbf4730b4d591ced15b902fe11f6cc399
Marcelo Henrique <marcelohs180@gmail.com>
2018-05-12 15:12:11 -0300

    add Asav info

--- src/components/AsavItem/AsavItem.js
+++ src/components/AsavItem/AsavItem.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Item from '../Item/Item'
+import Detail from '../Details/Detail'
 
 import img from './asav.png'
 
+class AsavDetails extends Component {
+  render () {
+    return (
+      <div>
+        <Detail
+          img={img}
+          title="Estágio Desenvolvedor Web"
+          text="Ajudei minha equipe a desenvolver e manter os sistemas da ASAV que cobrem a Unisinos, IHU e muitas outras instituições que atendemos. As principais tecnologias com as quais trabalhei foram PHP, MySQL, CSS, Javascript, JQuery e Lodash. Estive na equipe da Operação principalmente."
+          date="Fev 2018"
+        />
+      </div>
+    )
+  }
+}
+
 export default ({ showDetails }) => (
-  <div onClick={() => { showDetails("Asav") }}>
+  <div onClick={() => { showDetails(<AsavDetails />) }}>
     <Item title="Estágio Desenvolvedor Web" img={img} date="Fev 2018" />
   </div>
 )
