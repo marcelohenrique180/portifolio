b2cc779deb10a47bc1dc02fbed34b504777385d5
Marcelo Henrique <marcelohs180@gmail.com>
2018-05-13 15:24:26 -0300

    add social icons

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react'
+import ItemWrapper from '../Item/ItemWrapper'
+
 import './Social.css'
 
-export default class Social extends Component {
-  render() {
+class SocialDetails extends Component {
+  render () {
     return (
-      <div className="social">
-        <button className="social__text">Onde estou?</button>
-      </div>
+      <React.Fragment>
+        <ul className="sociald">
+          <li className="sociald__item">
+            <a href="https://www.linkedin.com/in/marcelo-bairros/" rel="noopener noreferrer" target="_blank" className="fa fa-linkedin"> </a>
+            <span>Linkedin</span>
+          </li>
+          <li className="sociald__item">
+            <a href="https://github.com/marcelohenrique180" rel="noopener noreferrer" target="_blank" className="fa fa-github"> </a>
+            <span>Github</span>
+          </li>
+          <li className="sociald__item">
+            <a href="mailto:marcelohs180@gmail.com" rel="noopener noreferrer" target="_blank" className="fa fa-google"> </a>
+            <span>marcelohs180@gmail.com</span>
+          </li>
+        </ul>
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default class Social extends Component {
+  render () {
+    return (
+      <ItemWrapper
+        render={
+          ({ showDetails }) => (
+            <div className="social">
+              <button className="social__text" onClick={() => showDetails(<SocialDetails />)}>
+                Onde estou?
+              </button>
+            </div>
+          )
+        }
+      ></ItemWrapper>
     )
   }
 }
